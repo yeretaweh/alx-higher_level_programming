f854da758c12478f474b94c3debb9191e00e4e5f
root <yeretaweh@gmail.com>
2024-02-12 15:58:29 -0800

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/node
-
-const complements = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (const complement of complements) {
-  console.log(complement);
-}
--- 0x12-javascript-warm_up/test-add.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-/*
-test the add function from the 13-add module
-*/
-
-const add = require('./13-add').add;
-
-console.log(add(12, 14));
